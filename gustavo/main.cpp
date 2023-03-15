31d2c6a2179b0ca51dd455129e2dfd73aa27cde2
Gustavo-Gs <113487971+Gustavo-Gs@users.noreply.github.com>
2023-03-15 10:59:46 -0300

    Add files via upload

---
+++ gustavo/main.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Contato {
+    string nome;
+    string numero;
+};
+
+int main() {
+    vector<Contato> agenda;
+    Contato primeiro;
+
+
+
+
+
+    return 0;
+}
